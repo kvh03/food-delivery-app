56484a1a930ae9d4efc4aa5cbcf84502cdc3358c
kvh <101593990+kvh03@users.noreply.github.com>
2024-10-12 19:26:30 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app config
+const app=express()
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+    console.log(req.method)
+})
+
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+});
+
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`)
+})
