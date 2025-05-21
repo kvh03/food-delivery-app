9756cea4921daf88cd820b18fd17503d705b46fb
kvh <101593990+kvh03@users.noreply.github.com>
2025-05-21 21:50:52 +0530

    Update StoreContext.js

--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = 'http://localhost:4000';
+    const url = 'https://food-delivery-app-1w8i.onrender.com';
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState(local_food_list);
 
